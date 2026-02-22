3e8cf7f33263adc5137fd2a3682b93f8e8cc82b7
Owen Neuillyr <jacquonneries@gmail.com>
2026-02-22 19:12:09 +0100

    stable version

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,19 @@ use std::usize;
 pub use pixels;
 use pixels::{Pixels, SurfaceTexture};
 pub use rgb;
+use std::cell::RefCell;
+use std::rc::Rc;
+#[cfg(not(target_arch = "wasm32"))]
+use std::time::Instant;
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen_futures::spawn_local;
+#[cfg(target_arch = "wasm32")]
+use web_time::Instant;
 pub use winit;
+#[cfg(target_arch = "wasm32")]
+use winit::platform::web::WindowExtWebSys;
 use winit::window::{Window, WindowId};
 
 /// Mapping for the keys that are recognized. They are centered an AZERTY keyboard's essential keys
@@ -54,10 +66,11 @@ impl std::convert::TryFrom<&winit::keyboard::Key> for Key {
 /// Everyting about the window. Pixels and Window are options because they
 /// are constructed on "resume" and cannot be construted earlier
 pub struct WinitHandler {
-    winfbx: Option<WinFbx>,
+    winfbx: Rc<RefCell<Option<WinFbx>>>,
+    init_done: bool,
     pub width: usize,
     pub height: usize,
-    last_frame: std::time::Instant,
+    last_frame: Instant,
     tick: std::time::Duration,
     /// Set to true if your app has something special to do at every tick even if there are no user
     /// events. This can be used if you have physics or an animation to run. Defaults to false to
@@ -87,10 +100,11 @@ impl WinitHandler {
     pub fn new(app: Box<dyn GfxApp>, size: (usize, usize), tick_per_second: u16) -> Self {
         let nsec_period = hz_to_nanosec_period(tick_per_second);
         Self {
-            winfbx: None,
+            winfbx: Rc::new(RefCell::new(None)), // Initialise vide
+            init_done: false,
             width: size.0,
             height: size.1,
-            last_frame: std::time::Instant::now(),
+            last_frame: Instant::now(),
             tick: std::time::Duration::from_nanos(nsec_period),
             app: Some(app),
             cursor_pos: (0.0, 0.0),
@@ -100,10 +114,7 @@ impl WinitHandler {
 
     pub fn run(&mut self) -> Result<(), winit::error::EventLoopError> {
         let event_loop = winit::event_loop::EventLoop::new()?;
-
-        // event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
         event_loop.set_control_flow(winit::event_loop::ControlFlow::Wait);
-
         event_loop.run_app(self)?;
         Ok(())
     }
@@ -114,48 +125,122 @@ impl WinitHandler {
     pub fn set_always_tick(&mut self, val: bool) {
         self.always_tick = val;
     }
+
+    pub fn new_with_winfbx(winfbx: WinFbx, size: (usize, usize), tick_per_second: u16) -> Self {
+        let nsec_period = hz_to_nanosec_period(tick_per_second);
+        Self {
+            winfbx: Rc::new(RefCell::new(Some(winfbx))), // Injection here
+            init_done: false,
+            width: size.0,
+            height: size.1,
+            last_frame: Instant::now(),
+            tick: std::time::Duration::from_nanos(nsec_period),
+            app: None, // App already in winfbx (could remove field)
+            cursor_pos: (0.0, 0.0),
+            always_tick: false,
+        }
+    }
 }
 
 impl winit::application::ApplicationHandler for WinitHandler {
     /// Resume gets called when window gets loaded for the first time
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        log::info!(".resumed() called, creating window");
+        if self.init_done {
+            return;
+        }
+        self.init_done = true;
+
+        log::info!("Moteur démarré. Création de la fenêtre...");
+
         if let Some(app) = self.app.take() {
-            self.winfbx = Some(WinFbx::new(event_loop, self.width, self.height, app));
+            let width = self.width;
+            let height = self.height;
+
+            let winfbx_handle = self.winfbx.clone();
+
+            let mut attr = Window::default_attributes()
+                .with_inner_size(winit::dpi::LogicalSize::new(width as f64, height as f64)) // LogicalSize to auto
+                .with_title("Chinchilib App");
+
+            #[cfg(target_arch = "wasm32")]
+            {
+                use winit::platform::web::WindowAttributesExtWebSys;
+                // Désactive le suivi automatique de la taille du canvas par winit
+                attr = attr.with_prevent_default(true);
+            }
+
+            let window = event_loop
+                .create_window(attr)
+                .expect("Impossible de créer la fenêtre");
+
+            #[cfg(target_arch = "wasm32")]
+            {
+                use web_sys::HtmlElement;
+                if let Some(canvas) = window.canvas() {
+                    let win = web_sys::window().unwrap();
+                    let doc = win.document().unwrap();
+
+                    if let Ok(canvas_el) = canvas.clone().dyn_into::<web_sys::HtmlCanvasElement>() {
+                        canvas_el.set_width(width as u32);
+                        canvas_el.set_height(height as u32);
+                        let _ = canvas_el
+                            .style()
+                            .set_property("width", &format!("{}px", width));
+                        let _ = canvas_el
+                            .style()
+                            .set_property("height", &format!("{}px", height));
+                    }
+
+                    let container = doc.get_element_by_id("wasm-canvas-container");
+
+                    if let Some(dst) = container {
+                        let _ = dst.append_child(&canvas);
+                        log::info!("Canvas injecté dans #wasm-canvas-container");
+                    } else {
+                        let _ = doc.body().unwrap().append_child(&canvas);
+                        log::info!("Canvas injecté dans body (container non trouvé)");
+                    }
+                }
+            }
+
+            #[cfg(target_arch = "wasm32")]
+            {
+                use wasm_bindgen_futures::spawn_local;
+                spawn_local(async move {
+                    log::info!("Initialisation Pixels (Async)...");
+                    let winfbx = WinFbx::new_async(window, width, height, app).await;
+
+                    *winfbx_handle.borrow_mut() = Some(winfbx);
+                    log::info!("Initialisation terminée !");
+                });
+            }
+
+            #[cfg(not(target_arch = "wasm32"))]
+            {
+                let winfbx = WinFbx::new(event_loop, width, height, app);
+                *winfbx_handle.borrow_mut() = Some(winfbx);
+            }
         }
     }
 
     /// Instead of redrawing for every event, or every keyprss, we only try to
     /// render after all evens have been processed.
     fn about_to_wait(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        let app = self
-            .winfbx
-            .as_mut()
-            .expect("about_to_wait not to be called if window doesn't exist.");
+        if let Ok(mut borrow) = self.winfbx.try_borrow_mut() {
+            if let Some(fbx) = borrow.as_mut() {
+                if fbx.done() {
+                    event_loop.exit();
+                    return;
+                }
 
-        if app.done() {
-            event_loop.exit();
-            return;
-        }
-        let now = std::time::Instant::now();
-        let duration_from_last_tick = now.duration_since(self.last_frame);
-        // If time since last tick is greater or equal than tickrate, we want to prompt that app
-        // for a redraw.
-        // Otherwise if they are key pressed we wait for next tick. If none are pressed we wait
-        // until we get an event.
-        // TODO: condition this behaviour to a flag
-        if duration_from_last_tick >= self.tick {
-            self.last_frame = now;
-            app.on_tick();
-            app.window.request_redraw();
-        } else {
-            if self.always_tick || !app.pressed_keys.is_empty() {
-                let duration_to_next_tick = self.tick - duration_from_last_tick;
-                event_loop.set_control_flow(winit::event_loop::ControlFlow::WaitUntil(
-                    now + duration_to_next_tick,
-                ));
-            } else {
-                event_loop.set_control_flow(winit::event_loop::ControlFlow::Wait);
+                let now = Instant::now();
+                if now.duration_since(self.last_frame) >= self.tick {
+                    self.last_frame = now;
+                    fbx.on_tick();
+                    fbx.window.request_redraw();
+                } else {
+                    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
+                }
             }
         }
     }
@@ -166,41 +251,36 @@ impl winit::application::ApplicationHandler for WinitHandler {
         _: WindowId,
         event: winit::event::WindowEvent,
     ) {
-        let app = self
-            .winfbx
-            .as_mut()
-            .expect("window_event not to be called if window doesn't exist.");
-        use winit::event::WindowEvent;
-        match event {
-            WindowEvent::CloseRequested => {
-                log::info!("The close button was pressed; stopping");
-                event_loop.exit();
+        if let Ok(mut borrow) = self.winfbx.try_borrow_mut() {
+            if let Some(fbx) = borrow.as_mut() {
+                use winit::event::WindowEvent;
+                match event {
+                    WindowEvent::CloseRequested => event_loop.exit(),
+                    WindowEvent::Resized(size) => {
+                        #[cfg(target_arch = "wasm32")]
+                        {
+                            if size.width > 0
+                                && size.width <= 4096
+                                && size.height > 0
+                                && size.height <= 4096
+                            {
+                                fbx.process_resize(size);
+                            }
+                        }
+                        #[cfg(not(target_arch = "wasm32"))]
+                        fbx.process_resize(size);
+                    }
+                    WindowEvent::KeyboardInput { event, .. } if !event.repeat => {
+                        fbx.process_kbd_input(event, event_loop)
+                    }
+                    WindowEvent::RedrawRequested => fbx.on_redraw(),
+                    WindowEvent::CursorMoved { position, .. } => {
+                        self.cursor_pos = (position.x, position.y);
+                    }
+                    WindowEvent::MouseInput { state, .. } if state.is_pressed() => {}
+                    _ => {}
+                }
             }
-            WindowEvent::Resized(size) => app.process_resize(size),
-            WindowEvent::KeyboardInput {
-                device_id: _,
-                event,
-                is_synthetic: _,
-            } if event.repeat == false => app.process_kbd_input(event, event_loop),
-            WindowEvent::RedrawRequested => app.on_redraw(),
-            WindowEvent::CursorMoved {
-                device_id: _,
-                position,
-            } => {
-                self.cursor_pos = (position.x, position.y);
-            }
-            WindowEvent::MouseInput {
-                device_id: _,
-                state,
-                button: _,
-            } if state.is_pressed() => {
-                log::info!(
-                    "clicked at x: {}, y: {}",
-                    self.cursor_pos.0,
-                    self.cursor_pos.1
-                )
-            }
-            _ => {}
         }
     }
 }
@@ -213,8 +293,8 @@ pub fn put_pixel(frame: &mut [u8], width: usize, x: usize, y: usize, color: rgb:
 
 /// Manages the actual winit::Window, the Pixels, handles resizes, records pressed keys into a
 /// custom structure and call the given app tick and draw methods.
-struct WinFbx {
-    window: Window,
+pub struct WinFbx {
+    pub window: Window,
     pixels: Pixels,
     pause: bool,
     height: usize,
@@ -226,7 +306,8 @@ struct WinFbx {
 }
 
 impl WinFbx {
-    fn new(
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn new(
         event_loop: &winit::event_loop::ActiveEventLoop,
         width: usize,
         height: usize,
@@ -235,20 +316,55 @@ impl WinFbx {
         let mut attr = Window::default_attributes();
         let size = winit::dpi::PhysicalSize::new(width as u16, height as u16);
         attr = attr.with_inner_size(size).with_title("Box");
-        let win = event_loop.create_window(attr).unwrap();
+        let window = event_loop.create_window(attr).unwrap();
 
-        let mut pixels = {
-            let surface_texture = SurfaceTexture::new(width as u32, height as u32, &win);
-            Pixels::new(width as u32, height as u32, surface_texture).unwrap()
-        };
+        let surface_texture = SurfaceTexture::new(width as u32, height as u32, &window);
+        let mut pixels = Pixels::new(width as u32, height as u32, surface_texture).unwrap();
+
         pixels.clear_color(pixels::wgpu::Color {
             r: 0.0,
             g: 0.0,
-            b: 255.0,
-            a: 255.0,
+            b: 0.0,
+            a: 1.0,
         });
+
         Self {
-            window: win,
+            window,
+            pixels,
+            height,
+            width,
+            pause: false,
+            pressed_keys: std::collections::HashSet::new(),
+            released_keys: std::collections::HashSet::new(),
+            needs_render: true,
+            app,
+        }
+    }
+
+    // VERSION WEB (Asynchrone) - Utilisée dans 'start_game' (runner)
+    #[cfg(target_arch = "wasm32")]
+    pub async fn new_async(
+        window: Window, // On reçoit la fenêtre déjà créée
+        width: usize,
+        height: usize,
+        app: Box<dyn GfxApp>,
+    ) -> Self {
+        let surface_texture = SurfaceTexture::new(width as u32, height as u32, &window);
+
+        // C'est ici qu'on utilise le .await qui posait problème
+        let mut pixels = Pixels::new_async(width as u32, height as u32, surface_texture)
+            .await
+            .expect("Pixels::new_async failed");
+
+        pixels.clear_color(pixels::wgpu::Color {
+            r: 0.0,
+            g: 0.0,
+            b: 0.0,
+            a: 1.0,
+        });
+
+        Self {
+            window,
             pixels,
             height,
             width,
@@ -263,12 +379,13 @@ impl WinFbx {
     fn on_redraw(&mut self) {
         if self.needs_render {
             self.app.draw(&mut self.pixels, self.width);
-
-            if let Err(err) = self.pixels.render() {
-                log::error!("failed to render with error {}", err);
-                return;
-            }
         }
+
+        if let Err(err) = self.pixels.render() {
+            log::error!("failed to render with error {}", err);
+            return;
+        }
+
         self.needs_render = false;
     }
 
@@ -278,9 +395,13 @@ impl WinFbx {
 
     fn on_tick(&mut self) {
         if self.app.done() == DoneStatus::NotDone {
-            self.needs_render = self
+            let app_requests_render = self
                 .app
                 .on_tick(&self.pressed_keys, (self.width, self.height));
+
+            if app_requests_render {
+                self.needs_render = true;
+            }
         }
         self.pressed_keys
             .retain(|candidate| !self.released_keys.contains(candidate));
@@ -312,10 +433,10 @@ impl WinFbx {
     }
 
     fn process_resize(&mut self, size: winit::dpi::PhysicalSize<u32>) {
-        self.width = size.width as usize;
-        self.height = size.height as usize;
-        self.pixels.resize_surface(size.width, size.height).unwrap();
-        self.pixels.resize_buffer(size.width, size.height).unwrap();
+        if let Err(err) = self.pixels.resize_surface(size.width, size.height) {
+            log::error!("resize_surface failed: {err}");
+            return;
+        }
         self.window.request_redraw();
         self.needs_render = true;
     }
